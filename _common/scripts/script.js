c81054d83bd8a33c673ba761ff3c589f9f4a0f76
TakaI2 <110tic2z@gmail.com>
2022-05-19 18:50:48 +0900

    音声再生のテスト

--- _common/scripts/script.js
+++ _common/scripts/script.js
@@ -324,12 +324,11 @@ function calcDistance(lat, lng) {
         //var pos = new google.maps.LatLng(CheckPoints[i].lat, CheckPoints[i].lng);                 // 人魚の位置を設定
         distances[i] = distance(lat, lng, CheckPoints[i].lat, CheckPoints[i].lng); //チェックポイントとの距離を測る
 
-        distances[1] = distances[0] + 2; 
-
-        if(distances[0] < 0.15){
+        /*
+        if(distances[0] < 0.015){
             searces = true;
         }
-
+        */
 
         var txt = document.getElementById("txt");       // データを表示するdiv要素の取得
                     txt.innerHTML = "ポイント1: " + distances[0] + ",  ポイント2:" + distances[1] +  ",  ポイント3:" + distances[2] + "<br>" 
@@ -338,10 +337,8 @@ function calcDistance(lat, lng) {
 
         // 捕獲の判定と捕獲した時のエフェクト
 
-
-
-        /*
-        if(distance[i] < 50 && captured[i] === false) {         // 距離が20m未満、かつ、まだ捕獲していないなら
+        
+        if(distance[i] < 0.15 && captured[i] === false) {         // 距離が20m未満、かつ、まだ捕獲していないなら
             searces = true;
             var music = new Audio(CheckPoints[i].sound);　　　　　　 // music変数をさくせい
             music.play();                                       // 音を流す            
@@ -358,7 +355,7 @@ function calcDistance(lat, lng) {
             });
         }
 
-        */
+        
     }
 }
 
