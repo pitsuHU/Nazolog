061991054ace6b2e0a821015238e1259efbcaa83
pitsuHU <pitsu_kyo_pro@yahoo.co.jp>
2022-07-28 11:27:50 +0900

    20220728

--- _common/scripts/script.js
+++ _common/scripts/script.js
@@ -366,12 +366,14 @@ function calcDistance(lat, lng) {
             get.hidden = false;                                 // img要素を表示
             mainDiv.hidden = true;                              // 地図を非表示
             portfolioDiv.hidden = true;                         // ポートフォリオを非表示
+            footerDiv.hidden = false;                        // footerを非表示
             initPlayer(CheckPoints[i]);
             closeButton.addEventListener("click", () => {            // img要素がクリックされたら
                 track.pause();
                 get.hidden = true;                           // img要素を非表示
                 mainDiv.hidden = false;                          // 地図を表示
                 portfolioDiv.hidden = false;                    //  ポートフォリオを表示
+                footerDiv.hidden = false;                        // footerを表示
             });
         }
 
